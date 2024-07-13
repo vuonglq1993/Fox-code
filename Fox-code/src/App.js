e2ad9e291dca349a02772c2f23feaf6468bb5cff
VuongLe <lqv1993@gmail.com>
2024-07-13 11:05:39 +0700

    â

--- Fox-code/src/App.js
+++ Fox-code/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './Fox-code/components/Header';
 import Staff from './Fox-code/staff';
 import Run1 from './Fox-code/run1';
 import Index from './Fox-code/HomePage';
 import Workwithus from './Fox-code/tienhuy-data/WorkWithUs';
+import AboutUs from './Fox-code/aboutus';
+
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
       <Routes>
         <Route path="/HomePage" element={<Index />} />
         <Route path="/staff" element={<Staff />} />
         <Route path="/run1" element={<Run1 />} />
-        {/* <Route path="/aboutUs" element={<AboutUs />} /> */}
+        <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/WorkWithUs" element={<Workwithus />} />
       </Routes>
     </div>
